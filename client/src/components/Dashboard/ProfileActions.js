7ec40b84528044c25e56ae352720fd42e6050c93
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-01-29 19:02:46 -0800

    fixed links

--- client/src/components/Dashboard/ProfileActions.js
+++ client/src/components/Dashboard/ProfileActions.js
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 const ProfileActions = () => {
   return (
     <div className="btn-group mb-4">
-      <a href="/edit-profile" className="btn btn-light">
+      <Link tp="/edit-profile" className="btn btn-light">
         <i class="fas fa-user-circle text-info mr-1" /> Edit Profile
-      </a>
+      </Link>
     </div>
   );
 };
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,17 +8,17 @@ class Landing extends Component{
       <div className="container">
         <div className="row">
           <div className="col-md-12 text-center">
-            <h1 className="display-3 mb-4">Quiz Share</h1>
+            <h1 className="display-3 mb-4">Two Truths and A Lie</h1>
             <p className="lead">
               {' '}
             </p>
             <hr />
-            <a href="/register" to="/register" className="btn btn-lg btn-info mr-2">
+            <Link to="/register" className="btn btn-lg btn-info mr-2">
               Sign Up
-            </a>
-            <a href="/login" to="/login" className="btn btn-lg btn-light">
+            </Link>
+            <Link to="/login" className="btn btn-lg btn-light">
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
