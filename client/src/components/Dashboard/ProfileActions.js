ed9b3dde67d8875f6db3261db17cda7d804a5927
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-01-29 19:16:52 -0800

    push

--- client/src/components/Dashboard/ProfileActions.js
+++ client/src/components/Dashboard/ProfileActions.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const ProfileActions = () => {
   return (
     <div className="btn-group mb-4">
-      <Link tp="/edit-profile" className="btn btn-light">
+      <Link to="/edit-profile" className="btn btn-light">
         <i class="fas fa-user-circle text-info mr-1" /> Edit Profile
       </Link>
     </div>
